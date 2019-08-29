b52c315fe3df00a62250bf0c35e553f141be23cf
Amberoot <48268788+HeyAmberoot@users.noreply.github.com>
2019-08-29 11:05:26 +0800

    Update AVCaptureMetadataOutputObjectsDelegate.swift

--- QRCode/AVCaptureMetadataOutputObjectsDelegate.swift
+++ QRCode/AVCaptureMetadataOutputObjectsDelegate.swift
@@ -1,9 +1,8 @@
 //
 //  AVCaptureMetadataOutputObjectsDelegate.swift
-//  learnRealm
 //
 //  Created by xww on 2018/11/19.
-//  Copyright © 2018年 cuanbo. All rights reserved.
+//  Copyright © 2018年 amberoot. All rights reserved.
 //
 
 import Foundation
