6853dff3a819aaf16bbb1c1ae4fc958011411aa9
Amberoot <48268788+HeyAmberoot@users.noreply.github.com>
2019-08-29 14:48:19 +0800

    Update xmlHelper.swift

--- xmlHelper.swift
+++ xmlHelper.swift
@@ -1,9 +1,8 @@
 //
 //  xmlHelper.swift
-//  CBC
 //
 //  Created by xww on 17/12/15.
-//  Copyright © 2017年 cuanbo. All rights reserved.
+//  Copyright © 2017年 amberoot. All rights reserved.
 //
 
 import Foundation
